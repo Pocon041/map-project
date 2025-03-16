103db59cba97c409ef1ec1193eadd3daaa3bf229
Pocon041 <1561061781@qq.com>
2025-03-16 15:18:15 +0800

    add scale

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,19 @@ export default function App() {
   const mapRef=useRef<HTMLDivElement>(null);
   useEffect(()=>{
     if(mapRef.current){
-      const map=L.map(mapRef.current).setView([36,120],7);
+      const map=L.map(mapRef.current,{
+        zoomControl:false,
+        attributionControl:false
+      }).setView([36,120],7);
+      L.control
+        .scale({
+          imperial:false,
+          position:"bottomright",
+        })
+        .addTo(map);
+
+      L.control.zoom({position:'bottomright'}).addTo(map),
+
       L.tileLayer(
         "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
         {
@@ -13,5 +25,7 @@ export default function App() {
       ).addTo(map);
     }
   },[]);
+  
+  
   return <div ref={mapRef} style={{height:"100vh"}}></div>;
 }

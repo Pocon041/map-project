4bd39c5949156555ba4208341f8a6c4a9f6178af
Pocon041 <1561061781@qq.com>
2025-03-23 16:09:32 +0800

    add canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useRef, useState } from "react";
-import L, { Control, map, Marker, popup } from "leaflet";
+import L, { Control, imageOverlay, map, Marker, popup } from "leaflet";
 import Location from "./components/Location.tsx"
 import store from "./store/index.ts"
 
 export default function App() {
   const mapRef=useRef<HTMLDivElement>(null);
-  //const [latLng, setLatLng] = useState<{ lat: number; lng: number } | null>(null);
   const [mapReady,setMapReady] = useState(false);
-  const map = useRef<L.Map>(null);
-
+  
   useEffect(()=>{
     getTempData();
+    
     if(mapRef.current){
       const _map=L.map(mapRef.current,{
         zoomControl:false,
         attributionControl:false
       }).setView([36,120],7);
       store.setMap(_map);
+      renderCanvas(_map);
       setMapReady(true);
 
       L.control
@@ -42,9 +42,35 @@ export default function App() {
   },[]);
   
   const getTempData = async()=>{
-    await fetch("/temp1.json");
+    const tempData: FieldData = await fetch("/temp1.json").then((res)=>
+      res.json()
+    );
+    console.log(tempData);
   };
 
+  const renderCanvas = async(map:L.Map)=>{
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext('2d')!;
+    ctx.fillStyle = "rgb(255,0,0)";
+    ctx.fillRect(10,10,55,50);
+    const url = canvas.toDataURL("png");
+    console.log(url);
+
+    const tempData: FieldData = await fetch("/temp1.json").then((res)=>res.json());
+
+    const imageBounds =/* L.LatLngBounds(*/[
+      [tempData.yllcorner,tempData.xllcorner],
+      [
+        tempData.yllcorner+tempData.nrows*tempData.cellsize,
+        tempData.xllcorner+tempData.ncols*tempData.cellsize,
+      ]
+    ]/*)*/;
+
+    const layer = L.imageOverlay(url,imageBounds).addTo(map);
+    console.log(layer);
+    //L.imageOverlay(url,imageBounds).addTo(map);
+  }
+  
   return (
     <div className="relative">
       <div ref={mapRef} className="z-10 h-svh"></div>
@@ -53,3 +79,12 @@ export default function App() {
     
   );
 }
+
+type FieldData = {
+  cellsize: number;
+  data: string[][];
+  ncols: number;
+  nrows: number;
+  xllcorner: number;
+  yllcorner: number; 
+};
